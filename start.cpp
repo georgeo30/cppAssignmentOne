3469ea98169e8f8c8a633bca99fa114e7f2af9e9
Georgeo <georgeosabu@gmail.com>
2020-02-14 16:01:46 +0200

    While loop to ask until q is pressed

--- start.cpp
+++ start.cpp
@@ -8,7 +8,15 @@ int main ()
   string mystr;
   cout << "Welcome to the student database \n0: Add Student\n1: Read/Save data\n2: Display student data\n3: Grade student\nq: Exit the program"<<endl;
   getline (cin, mystr);
-  cout << "Hello " << mystr << ".\n";
-  
+  while (mystr!="q"){
+
+    if(mystr=="0")
+    {
+            cout << "Hello " << mystr << ".\n";
+
+    }
+     cout << "Welcome to the student database \n0: Add Student\n1: Read/Save data\n2: Display student data\n3: Grade student\nq: Exit the program"<<endl;
+  getline (cin, mystr);
+  }
   return 0;
 }
