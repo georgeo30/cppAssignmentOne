33895e90625cb82d96c1b0a125a733d3a1e5a7bb
Georgeo <georgeosabu@gmail.com>
2020-02-17 22:15:07 +0200

    new header file for database

---
+++ database_h.h
@@ -0,0 +1,28 @@
+#ifndef start
+#define start
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+//using namespace std;
+namespace THNGEO002{
+    
+    //class myclass{
+       // public:
+       struct StudentRecord{
+                std::string fname, sname, sNumber,classRecord;
+            };
+       extern std::vector<StudentRecord> vectorArray;
+            void cScreen();
+            void addStudent();
+            void readData();
+            void saveData();
+            void displayStudent();
+            void gradeStudent();
+            
+
+            
+    //};
+}
+#endif
